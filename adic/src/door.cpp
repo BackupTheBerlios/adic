629ef519f665978022e7dc7b1c11b32ecefe9b58
karme <karme>
2002-06-09 13:42:20 +0000

    door should stop (earlier)

--- adic/src/door.cpp
+++ adic/src/door.cpp
@@ -16,9 +16,12 @@ Door::step(R dt)
   angle+=angleSpeed*dt;
   if (angle>maxAngle) {angle=maxAngle-(angle-maxAngle);angleSpeed*=-1;}
   if (angle<minAngle) {angle=minAngle+(minAngle-angle);angleSpeed*=-1;}
-  angleSpeed-=angleSpeed*damping*dt;
-  if (angleSpeed>RAD(90)) angleSpeed=RAD(90);
-  if (angleSpeed<RAD(-90)) angleSpeed=RAD(-90);
+  int sig1=(angleSpeed>0) ? 1 : ((angleSpeed<0) ? -1 : 0);
+  angleSpeed-=angleSpeed*damping*dt+R(sig1)*damping*dt;
+  int sig2=(angleSpeed>0) ? 1 : ((angleSpeed<0) ? -1 : 0);
+  if (sig1!=sig2) angleSpeed=0;
+  else if (angleSpeed>RAD(90)) angleSpeed=RAD(90);
+  else if (angleSpeed<RAD(-90)) angleSpeed=RAD(-90);
   return true;
 }
 
