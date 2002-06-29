f156ddef5d32346b4397955637f63bc5326d0b35
karme <karme>
2002-06-29 10:15:26 +0000

    initial version

---
+++ adic/src/playerdata.cpp
@@ -0,0 +1,6 @@
+#include "playerdata.h"
+
+PlayerData::PlayerData()
+  : maxspeed(60), rotspeed(1), acceleration(40), hdamping(2), vdamping(1), mass(1),
+    maxfitness(10), minfitness(0.3), endurance(80), type(0), r(30), recoverTime(30)
+{}
---
+++ adic/src/playerdata.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2002 Jens Thiele <jens.thiele@student.uni-tuebingen.de>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*!
+   \file playerdata.h
+   \brief class PlayerData
+   \author Jens Thiele
+*/
+
+#ifndef PLAYERDATA_H
+#define PLAYERDATA_H
+
+#include "typedefs.h"
+#include <vector>
+#include <string>
+
+//! fixed player data
+class PlayerData
+{
+public:
+  PlayerData();
+
+  template <typename Layer2>
+  inline void composite(Layer2 &layer2)
+  {
+    layer2.SIMPLE(maxspeed).SIMPLE(rotspeed).SIMPLE(acceleration).SIMPLE(hdamping).SIMPLE(vdamping)
+      .SIMPLE(mass).SIMPLE(maxfitness).SIMPLE(minfitness).SIMPLE(type)
+      .SIMPLE(r).SIMPLE(recoverTime);
+  }
+
+  //! maximum speed
+  R maxspeed;
+  //! rotation speed
+  R rotspeed;
+  //! acceleration
+  R acceleration;
+  //! damping of velocity not in our direction
+  R hdamping;
+  //! damping of velocity in our direction
+  R vdamping;
+  //! our mass
+  R mass;
+  //! maximum fitness
+  R maxfitness;
+  //! minimum fitness
+  R minfitness;
+  //! endurance / stamina (German: Ausdauer)
+  R endurance;
+  //! type (0=boy 1=girl 2=cowboy 3=gentleman 10=barrel 11=powerup)
+  int8_t type;
+  //! radius
+  R r;
+  //! how fast the player revocers
+  R recoverTime;
+protected:
+};
+DOPE_CLASS(PlayerData);
+template <typename Layer2>
+inline void composite(Layer2 &layer2, PlayerData &p)
+{
+  p.composite(layer2);
+}
+
+#endif
