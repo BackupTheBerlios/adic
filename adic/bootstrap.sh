8b83bfc9ea38bdd49387d04b72b8732993f2ce16
karme <karme>
2002-07-03 22:55:19 +0000

    fixes for next release

--- adic/bootstrap.sh
+++ adic/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+# Run this if you have to create the configure script
+# (f.e. you checked out from cvs)
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
@@ -9,6 +10,12 @@ PKG_NAME="game"
 (test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level directory"
+    exit 1
+}
+
+(test -f $srcdir/configure) && {
+    echo -n "**Error**: Directory "\`$srcdir\'" already contains"
+    echo " the configure script"
     exit 1
 }
 
