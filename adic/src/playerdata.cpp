a6685addd952d8615475fedc42589feef351db1b
karme <karme>
2002-07-08 23:29:36 +0000

    more action

--- adic/src/playerdata.cpp
+++ adic/src/playerdata.cpp
@@ -1,6 +1,6 @@
 #include "playerdata.h"
 
 PlayerData::PlayerData()
-  : maxspeed(75), minspeed(0.5), rotspeed(1), acceleration(50), hdamping(2), vdamping(1), mass(1),
+  : maxspeed(90), minspeed(0.5), rotspeed(1.5), acceleration(50), hdamping(2), vdamping(1), mass(1),
     maxfitness(10), minfitness(0.6), endurance(240), type(0), r(30), recoverTime(20)
 {}
