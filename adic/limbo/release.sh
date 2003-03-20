e13b6d9044309ce46531066d979eff8b89e7bf75
karme <karme>
2003-03-20 12:18:11 +0000

    *** empty log message ***

---
+++ adic/limbo/release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#configuration
+BUILDDIR=/tmp
+export ACLOCAL_FLAGS="-I config/m4"
+export CXX=g++-3.2
+PACKAGE=adic
+#end of configuration
+
+set -e
+
+cd $BUILDDIR
+
+#check that $PACKAGE directory does not already exist
+if test -e $PACKAGE; then
+	echo Warning $PACKAGE directory already exists - abort or I will remove it
+	read
+	rm -rf $PACKAGE
+fi
+
+#get source
+cvs -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE login
+cvs -z3 -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE co $PACKAGE
+
+#build distribution tarball
+cd $PACKAGE
+./bootstrap.sh
+./configure
+make dist
+DISTFILE=`ls -1 $PACKAGE-*.tar.gz`
+DIR=${DISTFILE%.tar.gz}
+if [ "x$DIR" = "x" ]; then
+    echo Fatal error
+    exit 1
+fi
+cd $BUILDDIR
+
+if test -e $DIR; then
+	echo Warning $DIR directory already exists - abort or I will remove it
+	read
+	rm -rf $DIR
+fi
+tar xzvf $PACKAGE/$DISTFILE
+cd $DIR
+dpkg-buildpackage -rfakeroot
