4da1e1d3dd6eb09d590692922f27bd2759dcb5b2
karme <karme>
2003-03-31 21:53:46 +0000

    *** empty log message ***

--- adic/limbo/release.sh
+++ adic/limbo/release.sh
@@ -47,4 +47,12 @@ if test -e $DIR; then
 fi
 tar xzvf $PACKAGE/$DISTFILE
 cd $DIR
+#build debian packages
 dpkg-buildpackage -rfakeroot
+
+# todo build static (or "semi-static") binary packages for linux
+# this builds a "semi-static" adicclient:
+#    semi-static because i had trouble with statically linked X libraries
+#g++-3.2 -Wall -ansi -pedantic -Wno-long-long -Os -o adicclient gamecommon.o netstream.o adicclient.o -nodefaultlibs  -L/tmp/usr/lib -L/usr/local/lib /tmp/usr/lib/libdope.a /usr/local/lib/libsigc.a /usr/lib/libxml2.a -L/usr/lib -L/usr/X11R6/lib /usr/lib/libSDL_image.a /usr/lib/libjpeg.a /usr/lib/libpng.a /usr/local/lib/libSDL_mixer.a /usr/lib/libSDL.a -lpthread -ldl /usr/lib/libesd.a /usr/lib/libaudiofile.a -lm -lX11 -lXext /usr/local/lib/gcc-lib/i586-pc-linux-gnu/3.2/libstdc++.a -lz -lgcc_s -lc
+
+#todo cross-compile for windows
