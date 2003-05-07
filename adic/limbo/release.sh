1e68c62e024d9be56f6cd38fac0908b9e10cd5e4
karme <karme>
2003-05-07 09:50:22 +0000

    prepare for next release

--- adic/limbo/release.sh
+++ adic/limbo/release.sh
@@ -12,6 +12,8 @@ CONFIGURE_OPTIONS="--with-dope-prefix=$PREFIX"
 export ADIC_DEB_CONFIGURE_OPTIONS=$CONFIGURE_OPTIONS
 DOPE_BUILD=$BUILDDIR/dope-release.sh
 ADIC_DEBREP=/mnt/schlumpf-pub/debian/adic/packages
+SETUPCROSS=~/develop/cross/config
+ADIC_CROSS_CONFIGURE_OPTIONS="--with-dope-prefix=$PREFIX/cross --disable-shared"
 #end of configuration
 
 set -e
@@ -43,37 +45,52 @@ cd $BUILDDIR
 
 #check that $PACKAGE directory does not already exist
 if test -e $PACKAGE; then
-	echo Warning $PACKAGE directory already exists - abort or I will remove it
-	read
-	rm -rf $PACKAGE
+    echo Warning $PACKAGE directory already exists - assuming we already have a distribution tarball in there
+    read
+else
+    #get source
+    cvs -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE login
+    cvs -z3 -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE co $PACKAGE
+
+    #build distribution tarball
+    cd $PACKAGE
+    ./bootstrap.sh
+    ./configure $CONFIGURE_OPTIONS
+    make dist
 fi
 
-#get source
-cvs -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE login
-cvs -z3 -d:pserver:anonymous:@cvs.$PACKAGE.berlios.de:/cvsroot/$PACKAGE co $PACKAGE
-
-#build distribution tarball
-cd $PACKAGE
-./bootstrap.sh
-./configure $CONFIGURE_OPTIONS
-make dist
+cd $BUILDDIR/$PACKAGE
 DISTFILE=`ls -1 $PACKAGE-*.tar.gz`
 DIR=${DISTFILE%.tar.gz}
 if [ "x$DIR" = "x" ]; then
     echo Fatal error
     exit 1
 fi
-cd $BUILDDIR
 
+cd $BUILDDIR
 if test -e $DIR; then
-	echo Warning $DIR directory already exists - abort or I will remove it
-	read
-	rm -rf $DIR
+    echo Warning $DIR directory already exists - assuming we already unpacked distribution
+    read
+else
+    tar xzvf $PACKAGE/$DISTFILE
 fi
-tar xzvf $PACKAGE/$DISTFILE
-cd $DIR
+
+#build in seperate directory
+mkdir -p build
+cd build
+
+if false; then
 
 #build debian packages
+mkdir -p debian
+if test -e $DIR; then
+    echo Warning $DIR directory already exists - abort or i will remove it
+    read
+    rm -rf $DIR
+fi
+cd $DIR
+tar xzvf $PACKAGE/$DISTFILE
+cd $DIR
 dpkg-buildpackage -rfakeroot
 
 #check debian packages
@@ -86,11 +103,30 @@ cd $ADIC_DEBREP/..
 ./gen-packages.sh
 ./update.sh
 
+fi
+
 # todo build static (or "semi-static") binary packages for linux/X11
 # this builds a "semi-static" adicclient:
 #    semi-static because i had trouble with statically linked X libraries
 #g++-3.2 -Wall -ansi -pedantic -Wno-long-long -Os -o adicclient gamecommon.o netstream.o adicclient.o -nodefaultlibs  -L/tmp/usr/lib -L/usr/local/lib /tmp/usr/lib/libdope.a /usr/local/lib/libsigc.a /usr/lib/libxml2.a -L/usr/lib -L/usr/X11R6/lib /usr/lib/libSDL_image.a /usr/lib/libjpeg.a /usr/lib/libpng.a /usr/local/lib/libSDL_mixer.a /usr/lib/libSDL.a -lpthread -ldl /usr/lib/libesd.a /usr/lib/libaudiofile.a -lm -lX11 -lXext /usr/local/lib/gcc-lib/i586-pc-linux-gnu/3.2/libstdc++.a -lz -lgcc_s -lc
 
-#todo cross-compile for windows
+#cross compile
+if test -e $SETUPCROSS; then
+    source $SETUPCROSS
+    cd $BUILDDIR/build
+    mkdir -p arch-cross
+    cd arch-cross
+    if test -e $DIR; then
+	echo Warning $DIR directory already exists - abort or I will remove it
+	read
+	rm -rf $DIR
+    fi
+    mkdir $DIR
+    cd $DIR
+    $BUILDDIR/$DIR/configure $CROSS_CONFIGURE_OPTIONS $ADIC_CROSS_CONFIGURE_OPTIONS --prefix=$PREFIX/cross --enable-fast-compile
+    make install
+else
+    echo cross compiler config script not found
+fi
 
 #todo build static binary packages for linux/fbdev
