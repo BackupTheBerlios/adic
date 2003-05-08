0c36d89a038072644a8dd381f3c350a50c0f9dff
karme <karme>
2003-05-08 09:55:11 +0000

    disable sound on error

--- adic/src/sound.cpp
+++ adic/src/sound.cpp
@@ -4,8 +4,18 @@
 Sound* 
 Sound::create(SoundConfig &sc)
 {
-  if (sc.sdriver=="sdlmixer")
-    return new SoundSDLMixer(sc);
+  if (sc.sdriver=="sdlmixer") {
+    // todo - I am not sure if this is correct
+    SoundSDLMixer* n=NULL;
+    try{
+      n=new SoundSDLMixer(sc);
+    }catch(const std::runtime_error &error){
+      std::cerr << error.what() << "\nsound disabled\n";
+      delete n;
+      n=NULL;
+    }
+    return new n;
+  }
   return NULL;
 }
 
